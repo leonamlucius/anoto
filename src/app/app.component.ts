93cba5338a34155ea9df3464aebaafc1567d59ca
leonamlucius <121642406+leonamlucius@users.noreply.github.com>
2026-05-13 11:16:57 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {TitleComponent} from './title/title.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, TitleComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'anoto';
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+@Component({
+  selector: 'app-sidebar',
+  imports: [NgFor],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+})
+export class SidebarComponent {
+  notes = [
+    { id: 1, title: 'Note 1', content: 'Content of Note 1', color: '#FFF176' },
+    { id: 2, title: 'Note 2', content: 'Content of Note 2', color: ' #F48FB1' },
+    { id: 3, title: 'Note 3', content: 'Content of Note 3', color: '#A5D6A7' },
+    { id: 4, title: 'Note 4', content: 'Content of Note 4', color: '#90CAF9' },
+    { id: 5, title: 'Note 5', content: 'Content of Note 5', color: '#FFCC80' },
+    { id: 6, title: 'Note 6', content: 'Content of Note 6', color: ' #CE93D8' },
+  ];
+
+  booleanValue = true;
+
+  public hiddeNoteContent() {
+    const colorsDiv = document.querySelector('.colors');
+    if (colorsDiv && this.booleanValue !== false) {
+      colorsDiv.classList.remove('active');
+    }
+  }
+
+  public showNoteContent() {
+    const colorsDiv = document.querySelector('.colors');
+    if (colorsDiv) {
+      colorsDiv.classList.add('active');
+    }
+  }
+
+  public toggleNoteContent() {
+    this.booleanValue = !this.booleanValue;
+    const colorsDiv = document.querySelector('.colors');
+    if (colorsDiv) {
+      colorsDiv.classList.toggle('active');
+    }
+  }
+}
