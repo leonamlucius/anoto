686f45278a891ccf998592648639014382258472
leonamlucius <leonam253@gmail.com>
2026-07-23 16:41:39 -0300

    [UPDATE] Teste jwt por inatividade

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DestroyRef } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { BodyComponent } from '../body/body.component';
 import { ServicesService } from '../services.service';
-import { timer } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { timer, merge, fromEvent, of } from 'rxjs';
+import { switchMap, throttleTime, startWith } from 'rxjs/operators';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
@@ -12,21 +13,40 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private servicesService: ServicesService) {}
+  constructor(
+    private servicesService: ServicesService,
+    private destroyRef: DestroyRef,
+  ) {}
 
   ngOnInit(): void {
     const token = localStorage.getItem('token');
 
+    if (!token) {
+      window.location.href = '/login';
+      return;
+    }
+
     const minutes = 30 * 60 * 1000;
 
-    timer(0, minutes)
+    const idleCheck$ = merge(
+      fromEvent(document, 'mousemove'),
+      fromEvent(document, 'keydown'),
+      fromEvent(document, 'click'),
+      fromEvent(document, 'scroll'),
+    ).pipe(throttleTime(1000), startWith(null));
+
+    const idleLoop$ = idleCheck$.pipe(switchMap(() => timer(minutes, minutes)));
+
+    merge(of(null), idleLoop$)
       .pipe(
         switchMap(() => {
           return this.servicesService.testToken(token);
         }),
+        takeUntilDestroyed(this.destroyRef),
       )
-      .subscribe((isValid) => {
+      .subscribe((isValid: boolean) => {
         if (!isValid) {
+          localStorage.removeItem('token');
           window.location.href = '/login';
         }
       });
